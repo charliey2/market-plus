d763cb3f59a3dcba25bdf8f523f1a790eae1d707
charliey2 <charliey2@users.noreply.github.com>
2023-04-11 16:52:57 +0000

    deploy: ab23a20bf09ebb60335027e62f46a7410603e930

--- javascripts/tags.js
+++ javascripts/tags.js
@@ -11,26 +11,17 @@ const style = `.tag {
     font-family: "Roboto";
     font-weight: normal;
 }
-.static {
-    background-color: rgb(38, 70, 83);
-}
 .read-only {
     background-color: rgb(42, 157, 143);
 }
 .client-only {
-    background-color: rgb(89, 140, 206);
+    background-color: rgb(82, 199, 95);
 }
 .server-only {
-    background-color: rgb(89, 140, 206);
+    background-color: rgb(57, 118, 204);
 }
 .no-change {
     background-color: rgb(85, 84, 102);
-}
-.toggleable {
-    background-color: rgb(178, 92, 162);
-}
-.chainable {
-    background-color: rgb(122, 103, 231);
 }
 .unstable {
     background-color: rgb(204, 134, 80);
@@ -44,14 +35,11 @@ h4 {
 
 var replaceStuff = [
 	["{read-only}", '<p class="tag read-only">read-only</p>'],
-	["{static}", '<p class="tag static">static</p>'],
     ["{server-only}", '<p class="tag server-only">server-only</p>'],
     ["{no-change}", '<p class="tag no-change">no-change</p>'],
 	["{client-only}", '<p class="tag client-only">client-only</p>'],
 	["{deprecated}", '<p class="tag deprecated">deprecated</p>'],
-	["{chainable}", '<p class="tag chainable">chainable</p>'],
 	["{unstable}", '<p class="tag unstable">unstable</p>'],
-	["{toggleable}", '<p class="tag toggleable">toggleable</p>'],
 ];
 
 function replace(element, from, to) {
